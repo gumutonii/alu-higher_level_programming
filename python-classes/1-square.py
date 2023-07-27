22c71c3e0a01c537e98d892dcacb73f96096b558
gumutonii <g.umutoni@alustudent.com>
2023-07-27 06:32:53 -0700

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is a file of a class with
+private attribute
+"""
+
+
+class Square:
+    """
+    Class with private attribute size
+    """
+    def __init__(self, size):
+        self.__size = size

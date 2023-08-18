f5ee6d9e13ef68299d7dcc456d9f7a2683103623
gumutonii <g.umutoni@alustudent.com>
2023-08-18 04:13:03 -0700

    task 8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
-"""
-    A script that lists all State objects from the database hbtn_0e_6_usa
-"""
-
-
+""" prints the first State object from the
+database hbtn_0e_6_usa"""
 import sys
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-
+from sqlalchemy.orm import Session
+from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-                        sys.argv[1], sys.argv[2], sys.argv[3]),
-                        pool_pre_ping=True
-                    )
-    Session = sessionmaker(bind=engine)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    session = Session()
+    session = Session(engine)
 
-    states = session.query(State).order_by(State.id).all()
+    record = session.query(State).first()
 
-    for state in states:
-        print(f"{state.id}: {state.name}")
+    if record:
+        print("{}: {}".format(record.__dict__['id'], record.__dict__['name']))
+    else:
+        print("Nothing")
 
     session.close()
